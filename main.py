2908b8d4f008d5956ff8999a32debea884e0a23b
nj4444 <130601453+hi-naresh@users.noreply.github.com>
2024-05-06 21:27:54 +0530

    added gunicorn

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ def api_get_activities():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5004)
+    app.run(debug=True)
